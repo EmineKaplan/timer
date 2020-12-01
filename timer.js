a6dbc15c3d96253678f17f09b0ac652f3bf1c04c
Emine Kaplan <eminekaplantr@gmail.com>
2020-12-01 03:15:52 +0000

    adding timer.js

---
+++ timer.js
@@ -0,0 +1,10 @@
+/*let arr = process.argv.slice(2).sort((a,b) => a-b);
+arr = arr.map(Number);
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] > 0 && Number((arr[i])))
+{
+  setTimeout(function(){console.log(arr[i]);}, arr[i]);
+}  }*/
+setTimeout(() => {
+  process.stdout.write('\x07');
+}, time * 1000);
